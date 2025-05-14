3927b0ace4fb33a01725e5d20464b5fd87a37abb
AbdulsamadAS2005 <ranaabdulsamad500@gmail.com>
2025-05-14 13:08:31 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+let express=require('express');
+let cors=require('cors');
+require('dotenv').config();
+let bodyParser=require('body-parser');
+
+let app=express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/gemini',async(req,res)=>{
+    let prompt=req.body.prompt;
+
+    let response=await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.API_KEY}`,
+        {
+            method:"POST",
+            headers:{
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                contents: [{ parts: [{ text: prompt }] }]
+            })
+        }
+    )
+    const data=await response.json();
+    res.send(data);
+    
+})
+
+
+app.listen(5000,()=>{
+    console.log('Server running on http://localhost:5000');
+})
