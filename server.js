9bcef0541496c6226d96926553a8b8b2db343fc0
AbdulsamadAS2005 <ranaabdulsamad500@gmail.com>
2025-06-07 16:43:07 +0500

    change

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let db = require('./connection');
 db();
 const mongoose = require('mongoose');
 app.get('/', async (req, res) => {
-    res.send(`tooliso.com...`);
+    res.send(`tooliso.com....`);
 })
 
 app.post('/gemini', async (req, res) => {
